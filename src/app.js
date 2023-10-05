dc1283e93fe17bc9510c7dfadefcd569c23a7d5b
Min.Hwan <hoangquan9851@gmail.com>
2023-10-05 22:27:54 +0700

    update code

--- src/app.js
+++ src/app.js
@@ -7,9 +7,13 @@ const upload = multer({ dest: 'uploads/' });
 const fs = require('fs');
 
 const app =express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const streamsController = require('./app/controllers/StreamsController');
+
+app.get("/", (req, res) => {
+    res.send('Hello World! nodejs server');
+});
 
 // get method to hls streaming file
 app.get('/etravel/live/hls/:folder/:filename', streamsController.HlsStreaming);
@@ -20,6 +24,6 @@ app.post('/upload',upload.single('mp3'), streamsController.convertFile);
 // delete method to delete file m3u8 from server
 app.delete('/remove', streamsController.RemoveFile);
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
